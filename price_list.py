1a9787dd6b51d8ba9e2666aecfb287ebe43516f3
mborek <01169148@pw.edu.pl>
2022-01-14 15:04:16 +0100

    added attributes and basic setters and getters to the PriceList class

--- price_list.py
+++ price_list.py
@@ -1,8 +1,36 @@
 from customers import SwimmingSchool, IndividualCustomer
+from work_hours import WorkHours
 
 
 class PriceList:
-    def __init__(self, price_list: dict):
+    def __init__(self, price_list: dict, hourly_price_school, hourly_price_individual, discount_price, discount_hours):
         self._price_list = price_list
+        self.set_hourly_price_school(hourly_price_school)
+        self.set_hourly_price_individual(hourly_price_individual)
+        self.set_discount_price(discount_price)
+        self.set_discount_hours(discount_hours)
 
+    def set_hourly_price_school(self, new_price):
+        self._hr_price_school = new_price
 
+    def hr_price_school(self):
+        return self._hr_price_school
+
+    def set_hourly_price_individual(self, new_price):
+        self._hr_price_individual = new_price
+
+    def hr_price_individual(self):
+        return self._hr_price_individual
+
+    def set_discount_price(self, dsc_price):
+        self._discount_price = dsc_price
+
+    def discount_price(self):
+        return self._discount_price
+
+    def set_discount_hours(self, discount_hours):
+        self._discount_hours = discount_hours
+
+    def discount_hours(self):
+        return self._discount_hours
+        
